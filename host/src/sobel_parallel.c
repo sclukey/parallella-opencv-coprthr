020199561255d10ffcaf37ec385963f7b3d96c38
Steven Clukey <saclukey@live.com>
2014-09-10 12:17:56 -0400

    Use shared memory with OpenCV. Also wait, but don't flush
    The IplImage data is now in shared memory, so there is no need to copy the
    memory into and out of the buffers into OpenCV objects.
    
    Also, the clwait call is now used instead of the clflush call.

--- host/src/sobel_parallel.c
+++ host/src/sobel_parallel.c
@@ -13,7 +13,27 @@ int main()
 {
 	struct timeval* start = (struct timeval*)malloc(sizeof(struct timeval));
 	struct timeval* end   = (struct timeval*)malloc(sizeof(struct timeval));
-	double* elapsed = (double*)malloc(sizeof(double));
+	double* elapsed       = (double*)malloc(sizeof(double));
+	unsigned int devnum;
+	IplImage* colorImage;
+	IplImage* grayImage;
+	IplImage* outputImage;
+
+	// ---------------------------
+	// Initialize the OpenCL stuff
+	// ---------------------------
+
+	// Use default contexts, if no ACCELERATOR use CPU
+	CLCONTEXT* cp = (stdacc) ? stdacc : stdcpu;
+
+	cl_kernel krn = clsym(cp, 0, "sobel_kern", 0);
+
+	// Define the computational domain and workgroup size
+	clndrange_t ndr = clndrange_init1d(0, 16, 16);
+
+	// ---------------------------
+	// Initialize the OpenCV stuff
+	// ---------------------------
 
 	CvCapture* cap = cvCaptureFromCAM(0);
 	if (!cap) {
@@ -21,34 +41,30 @@ int main()
 		exit(2);
 	}
 
-	IplImage* colorImage = cvQueryFrame(cap);
+	// Get an image from the camera to determine its size
+	colorImage = cvQueryFrame(cap);
 	if (!colorImage) {
 		fprintf(stderr, "Could not read from the camera\n");
 		exit(3);
 	}
-	IplImage* image = cvCreateImage(cvGetSize(colorImage), colorImage->depth, 1);
-	cvCvtColor(colorImage, image, CV_BGR2GRAY);
-	IplImage* outputImage = cvCreateImage(cvGetSize(image), image->depth, image->nChannels);
 
-	cl_uint width    = image->width;
-	cl_uint height   = image->height;
-	cl_uint channels = image->nChannels;
+	cl_uint width    = colorImage->width;
+	cl_uint height   = colorImage->height;
+	cl_uint channels = 1;
 	cl_uint n        = height*width*channels;
 	cl_uint line     = width*channels;
-	
-	unsigned int devnum;
-
-	// Use default contexts, if no ACCELERATOR use CPU
-	CLCONTEXT* cp = (stdacc) ? stdacc : stdcpu;
-
-	cl_kernel krn = clsym(cp, 0, "matvecmult_kern", 0);
 
 	// Allocate OpenCL device-sharable memory
-	cl_char* aa = (char*)clmalloc(cp, width*height*channels*sizeof(cl_char), 0);
-	cl_char* bb = (char*)clmalloc(cp, width*height*channels*sizeof(cl_char), 0);
+	cl_char* aa = (char*)clmalloc(cp, n*sizeof(cl_char), 0);
+	cl_char* bb = (char*)clmalloc(cp, n*sizeof(cl_char), 0);
 
-	// Define the computational domain and workgroup size
-	clndrange_t ndr = clndrange_init1d(0, 16, 16);
+	// Create OpenCV IplImage headers to hold the data
+	grayImage   = cvCreateImageHeader(cvGetSize(colorImage), colorImage->depth, 1);
+	outputImage = cvCreateImageHeader(cvGetSize(colorImage), colorImage->depth, 1);
+
+	// Use the shared buffers for the image data
+	cvSetData(grayImage,   aa, line);
+	cvSetData(outputImage, bb, line);
 
 	cvNamedWindow("Input", CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("Output", CV_WINDOW_AUTOSIZE);
@@ -56,42 +72,29 @@ int main()
 	while(1) {
 		gettimeofday(start, NULL);
 
-		printf("Getting the image from the camera\n");
+		// Getting the image from the camera
 		colorImage = cvQueryFrame(cap);
 
-		printf("Converting to gray\n");
-		cvCvtColor(colorImage, image, CV_BGR2GRAY);
+		// Converting to grayscale;
+		cvCvtColor(colorImage, grayImage, CV_BGR2GRAY);
 
-		// Initialize vector aa[] and zero bb[]
-		printf("Initializing\n");
-		int i,j;
-		for(i=0; i<n; i++) aa[i] = image->imageData[i];
-		//for(i=0; i<n; i++) bb[i] = '\0';
-		
 		// Non-blocking sync vectors a and b to device memory (copy to Epiphany)
-		printf("syncing to device\n");
 		clmsync(cp, devnum, aa, CL_MEM_DEVICE | CL_EVENT_NOWAIT);
 
 		// Non-blocking for of the OpenCL kernel to execute on the Epiphany
-		printf("Running the process\n");
 		clforka(cp, devnum, krn, &ndr, CL_EVENT_NOWAIT, n, line, aa, bb);
 
 		// Non blocking sync vector c to host memory (copy back to host)
-		printf("Syncing back to the host\n");
 		clmsync(cp, devnum, bb, CL_MEM_HOST | CL_EVENT_NOWAIT);
 
 		// Force execution of operations in command queue (non-blocking call)
-		printf("Flushing\n");
-		clflush(cp, devnum, 0);
+		//clflush(cp, devnum, 0);
 
 		// Block on completion of operations in command queue
-		//clwait(cp, devnum, CL_ALL_EVENT);
+		clwait(cp, devnum, CL_ALL_EVENT);
 
-		printf("Putting the output into the output image\n");
-		for(i=0; i<height*width*channels; i++) outputImage->imageData[i] = bb[i];
-
-		printf("Showing the images\n");
-		cvShowImage("Input", image);
+		// Showing the images
+		cvShowImage("Input", colorImage);
 		cvShowImage("Output", outputImage);
 
 		gettimeofday(end, NULL);
@@ -101,6 +104,8 @@ int main()
 
 		if (cvWaitKey(30) >= 0) break;
 	}
+
+	cvReleaseCapture(&cap);
 
 	clfree(aa);
 	clfree(bb);
