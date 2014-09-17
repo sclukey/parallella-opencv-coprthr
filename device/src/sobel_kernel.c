344dbaddbc78bcfb67d3730476ff4c6fbef4a9f3
Steven Clukey <saclukey@live.com>
2014-09-17 14:26:01 -0400

    kernel: Use tiles and move to local memory
    I now use 12 tiles to compute the sobel, each tile is 1600 pixels long.
    The pixels needed for each tile are brought into the local memory and the
    computation is done with the local versions. The outputs are stored in
    local memory also, but the memory is for all tiles. So the output memory
    is local to the kernel and global to the tiles.

--- device/src/sobel_kernel.c
+++ device/src/sobel_kernel.c
@@ -1,8 +1,8 @@
 __kernel void sobel_kern(
 	uint n,
 	uint line,
-	__global char* aa,
-	__global char* bb
+	__global char* gaa,
+	__global char* gbb
 	)
 {
 	int i,k;
@@ -17,20 +17,40 @@ __kernel void sobel_kern(
 
 	int tmp;
 
-	for(i=ifirst; i<iend; i++) {
-		tmp = abs(-     aa[i-line-1]
-		          - 2 * aa[i-1]
-		          -     aa[i+line-1]
-		          +     aa[i-line+1]
-		          + 2 * aa[i+1]
-		          +     aa[i+line+1])
-		    + abs(      aa[i-line-1]
-		          + 2 * aa[i-line]
-		          +     aa[i-line+1]
-		          -     aa[i+line-1]
-		          - 2 * aa[i+line]
-		          -     aa[i+line+1]);
-		bb[i] = tmp > 255 ? 255 : tmp;
+	char bb[iend-ifirst];
+
+	// Use 12 tiles for each block. The image is 640x480, so 1/16th of this image is 19,200 pixels (30 lines).
+	// This is split into 12, 1600 pixel (2.5 line) tiles. The data for each tile is moved into local memory.
+	// The entire output is stored in local memory.
+	int tile;
+	for (tile=0;tile<12;tile++)
+	{
+
+		char aa[2882]; // 1600 pixels, plus 1 line before and 1 line after, plus 2 extra border pixels
+
+		for(i=0;i<2882;i++)
+			aa[i] = gaa[ifirst+tile*1600+i-line-1];
+
+		for(i=line+1; i<2241; i++) {
+
+			tmp = abs(-     aa[i-line-1]
+			          - 2 * aa[i-1]
+			          -     aa[i+line-1]
+			          +     aa[i-line+1]
+			          + 2 * aa[i+1]
+			          +     aa[i+line+1])
+			    + abs(      aa[i-line-1]
+			          + 2 * aa[i-line]
+			          +     aa[i-line+1]
+			          -     aa[i+line-1]
+			          - 2 * aa[i+line]
+			          -     aa[i+line+1]);
+
+			bb[tile*1600+i-line-1] = tmp > 255 ? 255 : tmp;
+		}
 	}
+
+	for(i=ifirst; i<iend; i++)
+		gbb[i] = bb[i-ifirst];
 }
 
